353bb7f19578be40742d44438ff441c0108f417c
Eduardo Hamerski <hamerski@juno.com.br>
2019-11-23 12:18:47 -0200

    Initial version. Closes #1.

---
+++ integration-api-java-sdk/src/main/java/br/com/juno/App.java
@@ -0,0 +1,13 @@
+package br.com.juno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
